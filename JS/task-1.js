e65ffa3e90633a18cef30cbaa6a3f14356c8167f
Valeriy Voronets <Voronets.v@gmail.com>
2020-08-16 20:35:02 +0300

    done 1-1 1-4

---
+++ JS/task-1.js
@@ -0,0 +1,11 @@
+const name = '«Генератор защитного поля»'; // Write code on this line
+let price = 1000; // Write code on this line
+const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
+price = 2000;
+const secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
+
+console.log(firstMessage);
+
+price = 2000;
+
+console.log(secondMessage);
---
+++ JS/task-4.js
@@ -0,0 +1,34 @@
+const orderPieces = 10;
+
+const credits = 20000;
+const pricePerDroid = 3000;
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_DENIED = 'Недостаточно средств на счету!';
+
+let totalPrice = orderPieces * pricePerDroid;
+let balanceCredit = credits - totalPrice;
+let message;
+
+if (totalPrice <= credits) {
+  message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;
+} else {
+  message = ACCESS_DENIED;
+}
+
+if (orderPieces === null) {
+  message = CANCELED_BY_USER;
+}
+
+console.log(message);
+
+//если orderPieces равно 4
+// то значение message будет равно
+// 'Вы купили 4 дроидов, на счету осталось 11580 кредитов'
+
+//если orderPieces равно null
+// то значение message будет равно
+// 'Отменено пользователем!'
+
+//если orderPieces равно 10
+// то значение message будет равно
+// 'Недостаточно средств на счету!'
